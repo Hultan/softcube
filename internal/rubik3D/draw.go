26cecdcffc6ea7d0ad7860a775a9a53f50b262f9
Per Hultqvist <per@softteam.se>
2022-04-10 08:00:04 +0200

    animate

--- internal/rubik3D/draw.go
+++ internal/rubik3D/draw.go
@@ -1,6 +1,7 @@
 package rubik3D
 
 import (
+	"math"
 	"sort"
 
 	"github.com/gotk3/gotk3/cairo"
@@ -22,9 +23,19 @@ func (c *Cube) drawBackground(ctx *cairo.Context) {
 func (c *Cube) drawCube(ctx *cairo.Context) {
 	var cubits []Cubit
 
+	// Collect cubits
+	for i := 0; i < 27; i++ {
+		cubits = append(cubits, c.cubits[i])
+	}
+
+	// Animate
+	for i := 18; i < 27; i++ {
+		cubits[i] = cubits[i].rotate(0, 0, math.Pi/4)
+	}
+
 	// Rotate cubits
 	for i := 0; i < 27; i++ {
-		cubits = append(cubits, c.cubits[i].rotate(c.AngleX, c.AngleY, c.AngleZ))
+		cubits[i] = cubits[i].rotate(c.AngleX, c.AngleY, c.AngleZ)
 	}
 
 	// Sort by Z-coord
